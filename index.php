a21242a3e740aa51f8656c59a2d307912a17da3e
Kailesh Mistry <mistryk@cardiff.ac.uk>
2018-02-25 17:40:51 +0000

    Moved more code into anothre file, upload of brand new file

--- index.php
+++ index.php
@@ -79,38 +79,7 @@ session_start();
     <!-- #endregion Jssor Slider End -->
 </div>
 
-<script>
-    //Displaying first image
-    var slideIndex = 1;
-    showImg(slideIndex);
-
-    //Changes left and right clicks have on image displayed
-    function plusDivs(n) {
-        showImg(slideIndex += n);
-    }
-
-    //Displaying image at n position in list
-    function currentDiv(n) {
-        showImg(slideIndex = n);
-    }
-
-    //How image is displayed
-    function showImg(n) {
-    var i;
-    var x = document.getElementsByClassName("imgs");
-    var dots = document.getElementsByClassName("viewNum");
-    if (n > x.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = x.length}
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" white", "");
-    }
-    x[slideIndex-1].style.display = "block";  
-        dots[slideIndex-1].className += " white";
-    }
-</script>
+<script type="text/javascript" src="js/mainSliderBottom.js"></script>
 
 <br><br>
 
