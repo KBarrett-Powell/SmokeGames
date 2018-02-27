fbbdcccdea7a1b0f16e336178642b17de5e57e0a
kaileshmistry <MistryK@cardiff.ac.uk>
2018-02-27 01:42:25 +0000

    Working Gallery

--- index.php
+++ index.php
@@ -3,8 +3,16 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html><head>
-<title>Smoke Games</title>
-<link rel="stylesheet" type="text/css" href="Styles.css">
+    <title>Smoke Games</title>
+    <link rel="stylesheet" type="text/css" href="css/Styles.css">
+    <link rel="stylesheet" type="text/css" href="css/slider.css">
+
+    <!-- #region Jssor Slider Begin -->
+
+    <script src="js/jssor.slider-27.0.3.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="js/mainSlider.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,regular,500,600,700&subset=latin-ext,vietnamese,latin,cyrillic" rel="stylesheet" type="text/css" />
+
 </head><body>
 
 <!-- Creating Main Naviagtion Bar -->
@@ -28,20 +36,12 @@ session_start();
     echo"Hello ".$_SESSION['username']."!<br><br>";
 }?>
 
-<!-- Creating space for display of images -->
-<div class=imgDisplay>
-    <script src="js/jssor.slider-27.0.4.min.js" type="text/javascript"></script>
-
-    <!-- Code for the gallery slider. -->
-    <script type="text/javascript" src="js/mainSlider.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300italic,regular,italic,700,700italic&subset=latin-ext,greek-ext,cyrillic-ext,greek,vietnamese,latin,cyrillic" rel="stylesheet" type="text/css" />
-
-    <div id="jssor_1">
+<div id="jssor_1" style="position:relative;margin:0 auto;top:0px;left:0px;width:980px;height:380px;overflow:hidden;visibility:hidden;">
         <!-- Loading Screen -->
-        <div data-u="loading" class="jssorl-009-spin">
-            <img class="spinImg" src="img/spin.svg" />
+        <div data-u="loading" class="jssorl-009-spin" style="position:absolute;top:0px;left:0px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,0.7);">
+            <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="img/spin.svg" />
         </div>
-        <div data-u="slides" class="slidesImg">
+        <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:980px;height:380px;overflow:hidden;">
             <div data-p="225.00">
                 <img data-u="image" src="images/BannerImages/Ghost.jpg" />
             </div>
@@ -53,21 +53,21 @@ session_start();
             </div>
         </div>
         <!-- Bullet Navigator -->
-        <div data-u="navigator" class="jssorb032" data-autocenter="1" data-scale="0.5" data-scale-bottom="0.75">
-            <div data-u="prototype" class="i">
-                <svg viewbox="0 0 16000 16000" class="svgNav">
+        <div data-u="navigator" class="jssorb052" style="position:absolute;bottom:12px;right:12px;" data-autocenter="1" data-scale="0.5" data-scale-bottom="0.75">
+            <div data-u="prototype" class="i" style="width:16px;height:16px;">
+                <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
                     <circle class="b" cx="8000" cy="8000" r="5800"></circle>
                 </svg>
             </div>
         </div>
         <!-- Arrow Navigator -->
-        <div data-u="arrowleft" class="jssora051" data-autocenter="2" data-scale="0.75" data-scale-left="0.75">
-            <svg viewbox="0 0 16000 16000" class="arrowSvg">
+        <div data-u="arrowleft" class="jssora053" style="width:55px;height:55px;top:0px;left:25px;" data-autocenter="2" data-scale="0.75" data-scale-left="0.75">
+            <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
                 <polyline class="a" points="11040,1920 4960,8000 11040,14080 "></polyline>
             </svg>
         </div>
-        <div data-u="arrowright" class="jssora051" data-autocenter="2" data-scale="0.75" data-scale-right="0.75">
-            <svg viewbox="0 0 16000 16000" class="arrowSvg">
+        <div data-u="arrowright" class="jssora053" style="width:55px;height:55px;top:0px;right:25px;" data-autocenter="2" data-scale="0.75" data-scale-right="0.75">
+            <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
                 <polyline class="a" points="4960,1920 11040,8000 4960,14080 "></polyline>
             </svg>
         </div>
@@ -110,7 +110,6 @@ session_start();
     if (!$gamesdb) {
        die("Failed to connect: " . mysqli_connect_error());
     } 
-
     // This SQL query defines how the items are sorted and what they are filtered by
     if (isset($_GET['sortwhat']) && isset($_GET['filterby'])) {
         $retrieve = "SELECT * FROM Games WHERE Category LIKE '%".$_GET['filterby']."%' ORDER BY ". $_GET['sortwhat'] . " " . $_GET['sorthow'] . ";";
@@ -121,7 +120,6 @@ session_start();
     } else {    
         $retrieve = "SELECT * FROM Games ORDER BY Gname ASC;";
     }
-
     $result = mysqli_query($gamesdb, $retrieve);
     if (mysqli_num_rows($result) > 0) {
        echo "<br><div class='games'><div class='grid'>";
@@ -129,14 +127,12 @@ session_start();
            $id = $row["GameID"];
            $name = $row["Gname"];
            $img = $row["Gimg2"];
-
            // Display an image of the game above the name, user clicking on this takes them to another page with more info about the game
            echo "<a href='IndGame.php?id=$id'><div class='game'><img src='images/$img' alt='Could not find' class='gameimage'>";
            echo "<h2>$name</h2></a></div>";
        }
        echo "</div></div>";
     } else { echo "No results"; }
-
     mysqli_close($gamesdb);
 ?>
 </body></html>
